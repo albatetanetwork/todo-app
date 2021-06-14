1cf4a4b91538cbda5d5c218846c68a49d5114c05
Alba <albatetanetwork@gmail.com>
2021-06-13 22:14:00 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 
 
 const DATA = [
-  { id: "todo-0", name: "Eat", completed: true },
-  { id: "todo-1", name: "Sleep", completed: false },
-  { id: "todo-2", name: "Repeat", completed: false }
+  { id: "todo-0", name: "Yoga", completed: true },
+  { id: "todo-1", name: "Shopping", completed: false },
+  { id: "todo-2", name: "Complete Flatiron-school-phase2 project", completed: false }
 ];
 
 ReactDOM.render(
